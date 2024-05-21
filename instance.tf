d42ebfe38fb2902c9122d407a304793ae77bbc79
Manya Agarwal <141712702+Manyaagarwal19@users.noreply.github.com>
2024-05-21 13:24:41 +0530

    removing security group part from instance.tf

--- instance.tf
+++ instance.tf
@@ -1,28 +1,3 @@
-resource "aws_security_group" "allow_ssh" {
-  name = "allow_ssh"
-
-
-  dynamic "ingress" {
-    for_each = [22, 80]
-    iterator = port
-    content {
-      description = "shhforaws"
-      from_port   = port.value
-      to_port     = port.value
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-  }
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-}
-
-
 resource "aws_instance" "web01" {
   ami                    = "ami-0df435f331839b2d6"
   instance_type          = "t2.micro"
